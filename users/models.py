8894ff8559e608359a33c7ecfdc15de88ef1f189
Sergey Belyakov <belserg2002@gmail.com>
2026-09-20 21:11:25 +0300

    iniciate project

---
+++ users/models.py
@@ -0,0 +1,10 @@
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    preferred_category = models.ForeignKey(
+        'catalog.Category', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+',
+    )
